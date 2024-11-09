2b8c0d9f69422f75efd82d8b2fb4dc59e5c186ba
Saiprakash J <jsprakash632@gmail.com>
2024-11-10 05:24:02 +0530

    Update brutewebpaths.py

--- brutewebpaths.py
+++ brutewebpaths.py
@@ -45,7 +45,7 @@ class DirectoryBruteforce:
             try:
                 self.Thread_count = int(input("Enter the Number of Thread Count (between 0 - 9): "))
                 if (self.Thread_count >= 0 and self.Thread_count <= 9):
-                    break  
+                    break
                 else:
                     print("Invalid Thread Count, it must be between 0 and 9.")
 
@@ -55,18 +55,27 @@ class DirectoryBruteforce:
     def attack(self, fuzz):
 
         adding = str(self.url + '/' + fuzz.strip())
-        requesting = requests.get(adding)
+        requesting = requests.get(adding, timeout=8)
         try:
             if requesting.status_code == 200:
                 print(adding)
                 with open(f"200_{self.projectname}{1}.txt", "a") as file:
                     file.write(adding + "\n")
+            elif requesting.status_code == 302:
+                print(adding + "==REDIRECTION CODE(302)==")
+                with open(f"302_{self.projectname}.txt", "a") as file:
+                    file.write(adding + "\n")
+            elif requesting.status_code == 301:
+                print(adding + "  " + "==PERMANENT REDIRECTION CODE(301)==")
+                with open(f"301_{self.projectname}.txt", "a") as file:
+                    file.write(adding + "\n")
+
             elif requesting.status_code == 403:
                 print(adding)
                 with open(f"403_{self.projectname}.txt", "a") as file:
                     file.write(adding + "\n")
             elif requesting.status_code == 500:
-                print(adding)
+                print(adding + "  " + "==INTERNAL SERVER ERROR CODE(500)==")
                 with open(f"500_{self.projectname}.txt", "a") as file:
                     file.write(adding + "\n")
             elif requesting.status_code == 429:
@@ -85,14 +94,14 @@ class DirectoryBruteforce:
         except ConnectionError as e:
             print(f"connection error {e}")
 
-    import time
+
 
     def trail(self):
-        while True:  
-            self.user_choice = input("Enter a choice (exit/extend): ").strip().lower() 
+        while True:
+            self.user_choice = input("Enter a choice (exit/extend): ").strip().lower()
             if self.user_choice == 'exit':
                 print("Exited")
-                break  
+                break
             elif self.user_choice == 'extend':
                 try:
                     self.seconds = int(input("Enter the number of seconds to sleep: "))
@@ -109,9 +118,31 @@ class DirectoryBruteforce:
             with ThreadPoolExecutor(max_workers=self.Thread_count) as executor:
                 executor.map(self.attack, f)
 
+    def layer2(self, fuzz):
+        try:
+            with open(f"200_{self.projectname}{1}.txt", 'r') as file:
+                for line in file:
+                    a = line.strip() + '/' + fuzz.strip()
+                    try:
+                        request = requests.get(a, timeout=8)  # Add a timeout to avoid hanging indefinitely
+                        if request.status_code == 200:
+                            print(f"{a}")
+                    except requests.exceptions.RequestException as e:
+                        # Handle exceptions such as connection errors, timeouts, etc.
+                        print(f"Error with {a}: {e}")
+        except FileNotFoundError:
+            print("file Not found")
+
+
+    def Thread2(self):
+        with open(self.wordlist, 'r') as f:
+            with ThreadPoolExecutor(max_workers=self.Thread_count) as executor:
+                executor.map(self.layer2, f)
+
 
 dictionary = DirectoryBruteforce()
 dictionary.url_validations()
 dictionary.validating_wordlist()
 dictionary.Thread_Count_func()
 dictionary.Thread()
+dictionary.Thread2()
