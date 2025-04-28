17e2b0b5758c9cd10a045a8a949b6066adc580c2
Saiprakash J <jsprakash632@gmail.com>
2025-04-28 16:56:09 +0530

    Update brutewebpaths.py

--- brutewebpaths.py
+++ brutewebpaths.py
@@ -3,8 +3,9 @@ import requests
 import time
 from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import urlparse
-from colorama import init,Fore
+from colorama import init, Fore
 import argparse
+
 
 class DirectoryBruteforce:
     init(autoreset=True)
@@ -25,16 +26,16 @@ class DirectoryBruteforce:
         }
         self.arguments()
 
-
     def arguments(self):
         args = argparse.ArgumentParser(description="Directory Brute-Force Tool")
         args.add_argument("--url", type=str, help="Specify a URL.", required=True)
-        args.add_argument("--wordlist", type=str, help="Specify the wordlist path",required=True)
-        args.add_argument("--output", type=str, help="Specify the output file name (unique name is preferred)",required=True)
+        args.add_argument("--wordlist", type=str, help="Specify the wordlist path", required=True)
+        args.add_argument("--output", type=str, help="Specify the output folder name (unique name is preferred)",
+                          required=True)
         args.add_argument("--cookie", type=str, help="Specify the cookie inside string")
-        args.add_argument("--redirection", type=str, help="Specify whether redirection should be True or False (default is True)")
+        args.add_argument("--redirection", type=str,
+                          help="Specify whether redirection should be True or False (default is True)")
         args.add_argument("--thread", type=int, help="Specify the thread count (maximum 36, default is 7)")
-
 
         parsed_args = args.parse_args()
 
@@ -56,15 +57,12 @@ class DirectoryBruteforce:
                 exit()
 
         if self.Thread_count:
-
             try:
                 if (self.Thread_count >= 0 and self.Thread_count <= 36):
                     pass
-
                 else:
                     print("Invalid Thread Count, it must be between 0 and 36.")
                     exit()
-
             except ValueError:
                 print("Invalid input. Please enter an integer.")
 
@@ -85,6 +83,15 @@ class DirectoryBruteforce:
                 print("Invalid cookie format. Use key=value format.")
                 return
 
+        # Create the output folder
+        try:
+            if os.path.exists(self.projectname):
+                print(f"Folder '{self.projectname}' already exists!")
+                exit()
+            else:
+                os.mkdir(self.projectname)
+        except Exception as e:
+            print(e)
 
     def Extract_domain(self, domain):
         parsed_url = urlparse(domain)
@@ -125,17 +132,18 @@ class DirectoryBruteforce:
 
         if fuzz.startswith('.'):
             for trail in [fuzz, fuzz + '/']:
-                adding = str(self.url + fuzz)
+                adding = str(self.url + trail)
         elif '.' in fuzz[1:]:
             adding = str(self.url + fuzz + '/')
         else:
-            adding = str(self.url + fuzz+'/')
+            adding = str(self.url + fuzz + '/')
 
         if self.cookie is None:
             requesting = requests.get(adding, timeout=8, allow_redirects=True)
         if self.cookie is not None:
             requesting = requests.get(adding, timeout=8, allow_redirects=True, cookies=self.cookie)
         try:
+
             if requesting.history:
                 final_redirected_url = requesting.url
                 output = f"{adding} " + Fore.YELLOW + f"was redirected to {Fore.GREEN + final_redirected_url}"
@@ -143,21 +151,26 @@ class DirectoryBruteforce:
                 print(final_redirected_url)
                 domain_out = self.Extract_domain(final_redirected_url)
                 if self.domain == domain_out:
-                    with open(f"200_{self.projectname}{self.n}.txt", "a") as file:
-                        file.write(final_redirected_url + "\n")
+                    if final_redirected_url[-1] == '/':
+                        with open(os.path.join(self.projectname, f"200_{self.projectname}{self.n}.txt"), "a") as file:
+                            file.write(final_redirected_url + "\n")
+                    else:
+                        with open(os.path.join(self.projectname, f"302_{self.projectname}{self.n}.txt"), "a") as file:
+                            file.write(final_redirected_url + "\n")
             elif requesting.status_code == 200:
                 print(adding)
-                with open(f"200_{self.projectname}{self.n}.txt", "a") as file:
-                    file.write(adding + "\n" )
+                with open(os.path.join(self.projectname, f"200_{self.projectname}{self.n}.txt"), "a") as file:
+                    file.write(adding + "\n")
             elif requesting.status_code == 403 or requesting.status_code == 401:
-                with open(f"403_{self.projectname}{self.n}.txt", "a") as file:
+                with open(os.path.join(self.projectname, f"403_{self.projectname}{self.n}.txt"), "a") as file:
                     file.write(adding + "\n")
             elif requesting.status_code == 500:
-                print(adding + Fore.RED +"   STATUS CODE : 500   " )
-                with open(f"500_{self.projectname}.txt", "a") as file:
+                print(adding + Fore.RED + "   STATUS CODE : 500   ")
+                with open(os.path.join(self.projectname, f"500_{self.projectname}.txt"), "a") as file:
                     file.write(adding + "\n")
             elif requesting.status_code == 429:
-                print(Fore.RED + "Error 429: Too many requests have been made in a short period of time \n(sleeping for 600Sec.")
+                print(
+                    Fore.RED + "Error 429: Too many requests have been made in a short period of time \n(sleeping for 600Sec.")
                 time.sleep(600)
             else:
                 pass
@@ -172,22 +185,31 @@ class DirectoryBruteforce:
         except Exception as e:
             print(f"Error occur {e} ")
 
-    def layer2(self, fuzz, n):       #fuzz=wordlist n=file_count
-        with open(f"200_{self.projectname}{n}.txt", 'r') as file:           #opening saved urls
-            for urls in file:
+    def layer2(self, fuzz, n):  # fuzz=wordlist n=file_count
+        with open(os.path.join(self.projectname, f"200_{self.projectname}{n}.txt"), 'r') as file:  # opening saved urls
+            for line in file:
                 fuzz = fuzz.strip()
+
                 if fuzz == '/':
                     return
                 if fuzz.endswith('/'):
                     fuzz = fuzz.rstrip('/')
+                else:
+                    pass
 
-                url = urls.strip() + fuzz
+                if fuzz.startswith('.'):
+                    for trail in [fuzz, fuzz + '/']:
+                        url = str(line.strip() + trail)
+                elif '.' in fuzz[1:]:
+                    url = str(line.strip() + fuzz + '/')
+                else:
+                    url = str(line.strip() + fuzz + '/')
                 try:
                     if self.cookie is None:
                         response = requests.get(url, timeout=8, headers=self.header)
 
                     if self.cookie is not None:
-                        response = requests.get(url, timeout=8,headers = self.header, cookies=self.cookie)
+                        response = requests.get(url, timeout=8, headers=self.header, cookies=self.cookie)
 
                     if response.history:
                         final_redirected_url = response.url
@@ -196,18 +218,27 @@ class DirectoryBruteforce:
                         print(final_redirected_url)
                         domain_out = self.Extract_domain(final_redirected_url)
                         if self.domain == domain_out:
-                            with open(f"200_{self.projectname}{n + 1}.txt", "a") as value:
-                                value.write(final_redirected_url + "\n")
+                            if final_redirected_url[-1] == '/':
+                                with open(os.path.join(self.projectname, f"200_{self.projectname}{n + 1}.txt"),
+                                      "a") as value:
+                                    value.write(final_redirected_url + "\n")
+                            else:
+                                with open(os.path.join(self.projectname, f"302_{self.projectname}{n + 1}.txt"),
+                                      "a") as value:
+                                    value.write(final_redirected_url + "\n")
+
                     elif response.status_code == 200:
                         print(url)
-                        with open(f"200_{self.projectname}{n + 1}.txt", "a") as value:
+                        with open(os.path.join(self.projectname, f"200_{self.projectname}{n + 1}.txt"), "a") as value:
                             value.write(url + "\n")
                     elif response.status_code == 403 or response.status_code == 401:
-                        with open(f"403_{self.projectname}{self.n+1}.txt", "a") as value:
+                        with open(os.path.join(self.projectname, f"403_{self.projectname}{self.n + 1}.txt"),
+                                  "a") as value:
                             value.write(url + "\n")
                     elif response.status_code == 500:
                         print(url + Fore.RED + "   STATUS CODE : 500   ")
-                        with open(f"500_{self.projectname}{self.n+1}.txt", "a") as value:
+                        with open(os.path.join(self.projectname, f"500_{self.projectname}{self.n + 1}.txt"),
+                                  "a") as value:
                             value.write(url + "\n")
                 except requests.exceptions.RequestException as e:
                     pass
@@ -217,7 +248,7 @@ class DirectoryBruteforce:
 
         while True:
             try:
-                with open(f"200_{self.projectname}{self.n}.txt", 'r') as file:
+                with open(os.path.join(self.projectname, f"200_{self.projectname}{self.n}.txt"), 'r') as file:
                     y = file.read()
                     if not y:
                         break
